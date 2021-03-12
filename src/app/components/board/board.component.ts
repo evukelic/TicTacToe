2b90c595c2bbe476c65c6f83510752c1de9ff1b3
evukelic <evukeli@yahoo.com>
2021-03-12 22:44:27 +0100

    feat: State change on users click

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Move } from '../position/position.model';
 import { NUMBER_OF_POSITIONS } from './board.consts';
 
 @Component({
@@ -9,7 +10,18 @@ import { NUMBER_OF_POSITIONS } from './board.consts';
 export class BoardComponent implements OnInit {
   public moves: string[] = [...Array(NUMBER_OF_POSITIONS)].map((_) => '');
 
-  constructor() {}
+  public constructor() {}
 
-  ngOnInit(): void {}
+  public ngOnInit(): void {}
+
+  public getMove(move: string, position: number): Move {
+    return {
+      move,
+      position,
+    };
+  }
+
+  public onPlayerMove(index: number): void {
+    this.moves[index] = 'X';
+  }
 }
--- src/app/components/position/position.component.ts
+++ src/app/components/position/position.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Move } from './position.model';
 
 @Component({
   selector: 'app-position',
   templateUrl: './position.component.html',
-  styleUrls: ['./position.component.css']
+  styleUrls: ['./position.component.css'],
 })
 export class PositionComponent implements OnInit {
+  @Input() public move: Move;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
