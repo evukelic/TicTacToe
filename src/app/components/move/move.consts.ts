7a0033ac38a12daf365ef64cffd8cabc16514772
evukelic <evukeli@yahoo.com>
2021-03-18 22:29:42 +0100

    refactor: Export strings to const file

--- src/app/components/move/move.consts.ts
+++ src/app/components/move/move.consts.ts
@@ -3,3 +3,5 @@ export const COMP_SRC: string = '../../../assets/layout/o.jpg';
 
 export const PLAYER_WIN_SRC: string = '../../../assets/layout/x-win.jpg';
 export const COMP_WIN_SRC: string = '../../../assets/layout/o-win.jpg';
+
+export const PLAYER_MOVE: string = 'X';
