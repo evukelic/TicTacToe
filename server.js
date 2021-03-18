9ca846ad7bf1f95c8fc8f2957a34816181866b4a
evukelic <evukeli@yahoo.com>
2021-03-18 23:18:25 +0100

    fix: Fix deployment server

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/Tic-Tac-Toe'));
+app.use(express.static('./dist/tic-tac-toe'));
 
-app.get('/*', (req, res) => res.sendFile('index.html', { root: 'dist/Tic-Tac-Toe/' }));
+app.get('/*', (req, res) => res.sendFile('index.html', { root: 'dist/tic-tac-toe/' }));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
